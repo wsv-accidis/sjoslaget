42cc3d040e3bdacdc7486875cd25fdabf6313e5b
Wilhelm Svenselius <wilhelm.svenselius@gmail.com>
2017-02-25 13:55:37 +0100

    Initial OWIN migration.

---
+++ SjoslagetBackend/Startup.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Http;
+using Accidis.Sjoslaget.WebService;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Startup))]
+
+namespace Accidis.Sjoslaget.WebService
+{
+	public class Startup
+	{
+		public void Configuration(IAppBuilder app)
+		{
+			HttpConfiguration config = new HttpConfiguration();
+			WebApiConfig.Register(config);
+			app.UseWebApi(config);
+		}
+	}
+}
