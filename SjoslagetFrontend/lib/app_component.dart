be81c297e8b243770a882277857e9199a3f67792
Wilhelm Svenselius <wilhelm.svenselius@gmail.com>
2017-03-04 19:38:10 +0100

    Remove a bunch of useless comments.

--- SjoslagetFrontend/lib/app_component.dart
+++ SjoslagetFrontend/lib/app_component.dart
@@ -1,6 +1,3 @@
-// Copyright (c) 2017, wilhe. All rights reserved. Use of this source code
-// is governed by a BSD-style license that can be found in the LICENSE file.
-
 import 'package:angular2/core.dart';
 import 'package:angular2_components/angular2_components.dart';
 
--- SjoslagetFrontend/web/main.dart
+++ SjoslagetFrontend/web/main.dart
@@ -1,6 +1,3 @@
-// Copyright (c) 2017, wilhe. All rights reserved. Use of this source code
-// is governed by a BSD-style license that can be found in the LICENSE file.
-
 import 'package:angular2/platform/browser.dart';
 
 import 'package:Sjoslaget/app_component.dart';
