a05e056e15330592533e238a3162fed1f8477e49
Wilhelm Svenselius <wilhelm.svenselius@gmail.com>
2017-03-05 00:17:52 +0100

    Basic styling from the old site. Twitter/Facebook feeds.

--- SjoslagetFrontend/lib/app_component.dart
+++ SjoslagetFrontend/lib/app_component.dart
@@ -1,16 +1,14 @@
 import 'package:angular2/core.dart';
 import 'package:angular2/router.dart';
-import 'package:angular2_components/angular2_components.dart';
 
 import 'login_page/login_page.dart';
-import 'start_page/start_page.dart';
+import 'content_pages/start_page.dart';
 
 @Component(
   selector: 'sjoslaget-app',
   styleUrls: const ['app_component.css'],
   templateUrl: 'app_component.html',
-  directives: const [ROUTER_DIRECTIVES, materialDirectives, StartPage, LoginPage],
-  providers: const [materialProviders],
+  directives: const [ROUTER_DIRECTIVES]
 )
 @RouteConfig(const [
   const Route(path: '/', name: 'Start', component: StartPage, useAsDefault: true),
--- SjoslagetFrontend/lib/content_pages/start_page.dart
+++ SjoslagetFrontend/lib/content_pages/start_page.dart
@@ -2,8 +2,8 @@ import 'package:angular2/core.dart';
 
 @Component(
     selector: 'start-page',
+    styleUrls: const ['content_styles.css'],
     templateUrl: 'start_page.html'
 )
 class StartPage {
-
 }
