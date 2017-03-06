bf6dd706d0fac91d6b9a5c7664e1dcc0671009ec
Wilhelm Svenselius <wilhelm.svenselius@gmail.com>
2017-03-06 23:27:34 +0100

    Yet more content.

--- SjoslagetFrontend/lib/content_pages/about_booking_page.dart
+++ SjoslagetFrontend/lib/content_pages/about_booking_page.dart
@@ -1,10 +1,11 @@
 import 'package:angular2/core.dart';
-
+import 'package:angular2/router.dart';
 
 @Component(
 	selector: 'about-booking-page',
 	styleUrls: const ['content_styles.css'],
-	templateUrl: 'about_booking_page.html'
+	templateUrl: 'about_booking_page.html',
+	directives: const [ROUTER_DIRECTIVES]
 )
 class AboutBookingPage {
 }
