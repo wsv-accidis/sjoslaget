4f0bbb6dfea59d9f9b3f255bc5cdb87ea7d6eb39
Wilhelm Svenselius <wilhelm.svenselius@gmail.com>
2017-03-12 00:48:24 +0100

    Work on booking form.

--- SjoslagetFrontend/lib/booking/booking_details_component.dart
+++ SjoslagetFrontend/lib/booking/booking_details_component.dart
@@ -4,8 +4,14 @@ import 'package:angular2_components/angular2_components.dart';
 @Component(
 	selector: 'booking-details',
 	templateUrl: 'booking_details_component.html',
+	styleUrls: const ['../content/content_styles.css'],
 	directives: const [materialDirectives],
 	providers: const [materialProviders]
 )
 class BookingDetailsComponent {
+	String firstName;
+	String lastName;
+	String phoneNo;
+	String email;
+	bool acceptToc;
 }
--- SjoslagetFrontend/lib/content/booking_page.dart
+++ SjoslagetFrontend/lib/content/booking_page.dart
@@ -1,12 +1,14 @@
 import 'package:angular2/core.dart';
+import 'package:angular2/router.dart';
 
+import '../booking/booking_details_component.dart';
 import '../booking/booking_login_component.dart';
 
 @Component(
 	selector: 'booking-page',
 	styleUrls: const ['content_styles.css'],
 	templateUrl: 'booking_page.html',
-	directives: const [BookingLoginComponent]
+	directives: const [BookingDetailsComponent, BookingLoginComponent, ROUTER_DIRECTIVES]
 )
 class BookingPage {
 }
