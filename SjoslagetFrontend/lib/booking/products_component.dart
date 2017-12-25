wsv-accidis/sjoslaget

commit 1dd975b1364b90c04cf630fda4b7ae78ecb8c8be

Updated Angular to 4.0.0. Mass import/directive changes. (#13)

SjoslagetFrontend/lib/booking/products_component.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 
-import 'package:angular2/core.dart';
-import 'package:angular2_components/angular2_components.dart';
+import 'package:angular/angular.dart';
+import 'package:angular_components/angular_components.dart';
 
 import 'booking_addon_provider.dart';
 import 'booking_validator.dart';
@@ -16,7 +16,7 @@ import '../widgets/spinner_widget.dart';
 	selector: 'products-component',
 	templateUrl: 'products_component.html',
 	styleUrls: const ['../content/content_styles.css', 'products_component.css'],
-	directives: const <dynamic>[materialDirectives, SpinnerWidget],
+	directives: const <dynamic>[CORE_DIRECTIVES, materialDirectives, SpinnerWidget],
 	providers: const <dynamic>[materialProviders]
 )
 class ProductsComponent implements BookingAddonProvider, OnInit {

SjoslagetFrontend/lib/admin/admin_booking_list_page.dart
@@ -1,8 +1,8 @@
 import 'dart:async';
 
-import 'package:angular2/core.dart';
-import 'package:angular2/router.dart';
-import 'package:angular2_components/angular2_components.dart';
+import 'package:angular/angular.dart';
+import 'package:angular_components/angular_components.dart';
+import 'package:angular_router/angular_router.dart';
 import 'package:decimal/decimal.dart';
 import 'package:quiver/strings.dart' show isNotEmpty;
 
@@ -19,7 +19,7 @@ import '../util/datetime_formatter.dart';
 	selector: 'admin-booking-list-page',
 	templateUrl: 'admin_booking_list_page.html',
 	styleUrls: const ['../content/content_styles.css', 'admin_styles.css', 'admin_booking_list_page.css'],
-	directives: const<dynamic>[ROUTER_DIRECTIVES, materialDirectives, BookingPreviewComponent, SortableColumnHeader, SortableColumns, SpinnerWidget],
+	directives: const<dynamic>[CORE_DIRECTIVES, ROUTER_DIRECTIVES, materialDirectives, BookingPreviewComponent, SortableColumnHeader, SortableColumns, SpinnerWidget],
 	providers: const <dynamic>[materialProviders]
 )
 class AdminBookingListPage implements OnInit {

SjoslagetFrontend/lib/admin/admin_booking_page.dart
@@ -1,8 +1,9 @@
 import 'dart:async';
 
-import 'package:angular2/core.dart';
-import 'package:angular2/router.dart';
-import 'package:angular2_components/angular2_components.dart';
+import 'package:angular/angular.dart';
+import 'package:angular_components/angular_components.dart';
+import 'package:angular_forms/angular_forms.dart';
+import 'package:angular_router/angular_router.dart';
 import 'package:decimal/decimal.dart';
 import 'package:quiver/strings.dart' show isNotEmpty;
 
@@ -30,7 +31,7 @@ import '../widgets/spinner_widget.dart';
 	selector: 'admin-booking-page',
 	templateUrl: 'admin_booking_page.html',
 	styleUrls: const ['../content/content_styles.css', 'admin_styles.css', 'admin_booking_page.css'],
-	directives: const<dynamic>[materialDirectives, ROUTER_DIRECTIVES, CabinsComponent, ModalDialog, ProductsComponent, SpinnerWidget],
+	directives: const<dynamic>[CORE_DIRECTIVES, ROUTER_DIRECTIVES, formDirectives, materialDirectives, CabinsComponent, ModalDialog, ProductsComponent, SpinnerWidget],
 	providers: const<dynamic>[materialProviders]
 )
 class AdminBookingPage implements OnInit {

SjoslagetFrontend/lib/admin/admin_component.dart
@@ -1,5 +1,5 @@
-import 'package:angular2/core.dart';
-import 'package:angular2/router.dart';
+import 'package:angular/angular.dart';
+import 'package:angular_router/angular_router.dart';
 
 import 'admin_booking_page.dart';
 import 'admin_booking_list_page.dart';

SjoslagetFrontend/lib/admin/admin_dashboard_page.dart
@@ -1,8 +1,8 @@
 import 'dart:async';
 
-import 'package:angular2/core.dart';
-import 'package:angular2/router.dart';
-import 'package:angular2_components/angular2_components.dart';
+import 'package:angular/angular.dart';
+import 'package:angular_components/angular_components.dart';
+import 'package:angular_router/angular_router.dart';
 import 'package:oauth2/oauth2.dart' show ExpirationException;
 
 import '../booking/availability_component.dart';
@@ -18,7 +18,7 @@ import '../widgets/spinner_widget.dart';
 	selector: 'admin-dashboard-page',
 	templateUrl: 'admin_dashboard_page.html',
 	styleUrls: const ['../content/content_styles.css', 'admin_styles.css', 'admin_dashboard_page.css'],
-	directives: const<dynamic>[ROUTER_DIRECTIVES, materialDirectives, AvailabilityComponent, SpinnerWidget],
+	directives: const<dynamic>[CORE_DIRECTIVES, ROUTER_DIRECTIVES, materialDirectives, AvailabilityComponent, SpinnerWidget],
 	providers: const<dynamic>[materialProviders]
 )
 class AdminDashboardPage implements OnInit, OnDestroy {

SjoslagetFrontend/lib/admin/admin_export_page.dart
@@ -1,9 +1,10 @@
 import 'dart:async';
 import 'dart:html';
 
-import 'package:angular2/core.dart';
-import 'package:angular2/router.dart';
-import 'package:angular2_components/angular2_components.dart';
+import 'package:angular/angular.dart';
+import 'package:angular_components/angular_components.dart';
+import 'package:angular_forms/angular_forms.dart';
+import 'package:angular_router/angular_router.dart';
 import 'package:quiver/strings.dart' as str show isBlank, isNotEmpty;
 
 import '../client/client_factory.dart';
@@ -16,7 +17,7 @@ import '../widgets/spinner_widget.dart';
 	selector: 'admin-export-page',
 	templateUrl: 'admin_export_page.html',
 	styleUrls: const ['../content/content_styles.css', 'admin_styles.css', 'admin_export_page.css'],
-	directives: const<dynamic>[ROUTER_DIRECTIVES, materialDirectives, SpinnerWidget],
+	directives: const<dynamic>[CORE_DIRECTIVES, ROUTER_DIRECTIVES, formDirectives, materialDirectives, SpinnerWidget],
 	providers: const<dynamic>[materialProviders]
 )
 class AdminExportPage {

SjoslagetFrontend/lib/admin/admin_login_page.dart
@@ -1,8 +1,9 @@
 import 'dart:async';
 
-import 'package:angular2/core.dart';
-import 'package:angular2/router.dart';
-import 'package:angular2_components/angular2_components.dart';
+import 'package:angular/angular.dart';
+import 'package:angular_components/angular_components.dart';
+import 'package:angular_forms/angular_forms.dart';
+import 'package:angular_router/angular_router.dart';
 
 import '../client/client_factory.dart';
 import '../widgets/modal_dialog.dart';
@@ -11,7 +12,7 @@ import '../widgets/modal_dialog.dart';
 	selector: 'admin-login-page',
 	styleUrls: const ['../content/content_styles.css'],
 	templateUrl: 'admin_login_page.html',
-	directives: const<dynamic>[ROUTER_DIRECTIVES, materialDirectives, ModalDialog],
+	directives: const<dynamic>[CORE_DIRECTIVES, ROUTER_DIRECTIVES, formDirectives, materialDirectives, ModalDialog],
 	providers: const<dynamic>[materialProviders]
 )
 class AdminLoginPage {

SjoslagetFrontend/lib/admin/admin_pax_list_page.dart
@@ -1,8 +1,8 @@
 import 'dart:async';
 
-import 'package:angular2/core.dart';
-import 'package:angular2/router.dart';
-import 'package:angular2_components/angular2_components.dart';
+import 'package:angular/angular.dart';
+import 'package:angular_components/angular_components.dart';
+import 'package:angular_router/angular_router.dart';
 import 'package:quiver/strings.dart' show isNotEmpty;
 
 import '../client/booking_repository.dart';
@@ -17,7 +17,7 @@ import '../widgets/spinner_widget.dart';
 	selector: 'admin-pax-list-page',
 	templateUrl: 'admin_pax_list_page.html',
 	styleUrls: const ['../content/content_styles.css', 'admin_styles.css', 'admin_pax_list_page.css', '../booking/cabins_gender_field.css'],
-	directives: const<dynamic>[ROUTER_DIRECTIVES, materialDirectives, SortableColumnHeader, SortableColumns, SpinnerWidget],
+	directives: const<dynamic>[CORE_DIRECTIVES, ROUTER_DIRECTIVES, materialDirectives, SortableColumnHeader, SortableColumns, SpinnerWidget],
 	providers: const<dynamic>[materialProviders]
 )
 class AdminPaxListPage implements OnInit {

commit f91f67fa40100ea5b4032859fb4e6ca8ccf09b2a

Fix missing formDirectives in product component (caused by #13).

SjoslagetFrontend/lib/booking/products_component.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:angular/angular.dart';
 import 'package:angular_components/angular_components.dart';
+import 'package:angular_forms/angular_forms.dart';
 
 import 'booking_addon_provider.dart';
 import 'booking_validator.dart';
@@ -16,7 +17,7 @@ import '../widgets/spinner_widget.dart';
 	selector: 'products-component',
 	templateUrl: 'products_component.html',
 	styleUrls: const ['../content/content_styles.css', 'products_component.css'],
-	directives: const <dynamic>[CORE_DIRECTIVES, materialDirectives, SpinnerWidget],
+	directives: const <dynamic>[CORE_DIRECTIVES, formDirectives, materialDirectives, SpinnerWidget],
 	providers: const <dynamic>[materialProviders]
 )
 class ProductsComponent implements BookingAddonProvider, OnInit {
@@ -37,9 +38,9 @@ class ProductsComponent implements BookingAddonProvider, OnInit {
 
 	bool get isLoaded => null != bookingProducts;
 
-	bool get isValid => bookingProducts.every((p) => p.isValid);
+	bool get isValid => isLoaded && bookingProducts.every((p) => p.isValid);
 
-	int get price => bookingProducts.fold(0, (sum, p) => sum + p.price * ValueConverter.toInt(p.quantity));
+	int get price => isLoaded ? bookingProducts.fold(0, (sum, p) => sum + p.price * ValueConverter.toInt(p.quantity)) : 0;
 
 	ProductsComponent(this._bookingValidator, this._clientFactory, this._cruiseRepository);
 