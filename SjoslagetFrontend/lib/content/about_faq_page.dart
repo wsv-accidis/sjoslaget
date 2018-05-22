a142f16669fb3ee9d614461e7a653f1d07031d65
Wilhelm Svenselius <wsv@spotify.com>
2018-05-22 15:59:58 +0200

    Content updates.

--- SjoslagetFrontend/lib/content/about_faq_page.dart
+++ SjoslagetFrontend/lib/content/about_faq_page.dart
@@ -1,9 +1,11 @@
 import 'package:angular/angular.dart';
+import 'package:angular_router/angular_router.dart';
 
 @Component(
 	selector: 'about-faq-page',
 	styleUrls: const ['content_styles.css'],
-	templateUrl: 'about_faq_page_sj.html'
+	templateUrl: 'about_faq_page_sj.html',
+	directives: const <dynamic>[ROUTER_DIRECTIVES]
 )
 class AboutFaqPage {
 }
