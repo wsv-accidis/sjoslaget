28376a13f2f29e3aa415f3073f75d2feb9fb64be
Wilhelm Svenselius <wsv@spotify.com>
2021-05-30 22:29:51 +0200

    Add subcruise to booking preview popup.

--- SjoslagetFrontend/lib/admin/booking_preview_component.dart
+++ SjoslagetFrontend/lib/admin/booking_preview_component.dart
@@ -12,104 +12,99 @@ import '../model/booking_overview_item.dart';
 import '../model/booking_source.dart';
 import '../model/cruise_cabin.dart';
 import '../model/cruise_product.dart';
+import '../model/sub_cruise.dart';
 import '../model/summary_view.dart';
 import '../widgets/components.dart';
 import '../widgets/spinner_widget.dart';
 import 'admin_routes.dart';
 
 @Component(
-	selector: 'booking-preview-component-popup',
-	templateUrl: 'booking_preview_component_popup.html',
-	styleUrls: ['../content/content_styles.css', 'booking_preview_component.css'],
-	directives: <dynamic>[coreDirectives, routerDirectives, MaterialIconComponent, SpinnerWidget],
-	exports: <dynamic>[AdminRoutes]
-)
+    selector: 'booking-preview-component-popup',
+    templateUrl: 'booking_preview_component_popup.html',
+    styleUrls: ['../content/content_styles.css', 'booking_preview_component.css'],
+    directives: <dynamic>[coreDirectives, routerDirectives, MaterialIconComponent, SpinnerWidget],
+    exports: <dynamic>[AdminRoutes])
 class BookingPreviewComponentPopup implements OnInit {
-	final BookingRepository _bookingRepository;
-	final ClientFactory _clientFactory;
-	final CruiseRepository _cruiseRepository;
-	List<CruiseCabin> _cabins;
-	List<CruiseProduct> _products;
+  final BookingRepository _bookingRepository;
+  final ClientFactory _clientFactory;
+  final CruiseRepository _cruiseRepository;
+  List<CruiseCabin> _cabins;
+  List<CruiseProduct> _products;
 
-	BookingSource booking;
+  BookingSource booking;
 
-	bool get hasInternalNotes => null != booking && isNotEmpty(booking.internalNotes);
+  String formatSubCruise(String code) => SubCruise.codeToLabel(code);
 
-	@Input()
-	BookingOverviewItem bookingItem;
+  bool get hasInternalNotes => null != booking && isNotEmpty(booking.internalNotes);
 
-	BookingPreviewComponentPopup(this._bookingRepository, this._clientFactory, this._cruiseRepository);
+  @Input()
+  BookingOverviewItem bookingItem;
 
-	List<SummaryView> get cabinSummary {
-		final result = <SummaryView>[];
-		if (null == booking || null == _cabins)
-			return result;
+  BookingPreviewComponentPopup(this._bookingRepository, this._clientFactory, this._cruiseRepository);
 
-		for (CruiseCabin cabinType in _cabins) {
-			final count = booking.cabins
-				.where((bc) => bc.cabinTypeId == cabinType.id)
-				.length;
-			if (count > 0)
-				result.add(SummaryView(cabinType.id, cabinType.name, count));
-		}
+  List<SummaryView> get cabinSummary {
+    final result = <SummaryView>[];
+    if (null == booking || null == _cabins) return result;
 
-		return result;
-	}
+    for (CruiseCabin cabinType in _cabins) {
+      final count = booking.cabins.where((bc) => bc.cabinTypeId == cabinType.id).length;
+      if (count > 0) result.add(SummaryView(cabinType.id, cabinType.name, count));
+    }
 
-	bool get isLoaded => null != booking;
+    return result;
+  }
 
-	List<SummaryView> get productSummary {
-		final result = <SummaryView>[];
-		if (null == booking || null == _products)
-			return result;
+  bool get isLoaded => null != booking;
 
-		for (CruiseProduct productType in _products) {
-			final bookingProduct = booking.products.firstWhere(
-					(bp) => bp.productTypeId == productType.id, orElse: () => null);
-			if (null != bookingProduct && bookingProduct.quantity > 0)
-				result.add(SummaryView(productType.id, productType.name, bookingProduct.quantity));
-		}
+  List<SummaryView> get productSummary {
+    final result = <SummaryView>[];
+    if (null == booking || null == _products) return result;
 
-		return result;
-	}
+    for (CruiseProduct productType in _products) {
+      final bookingProduct = booking.products.firstWhere((bp) => bp.productTypeId == productType.id, orElse: () => null);
+      if (null != bookingProduct && bookingProduct.quantity > 0)
+        result.add(SummaryView(productType.id, productType.name, bookingProduct.quantity));
+    }
 
-	bool get hasProducts => null != booking && booking.products.isNotEmpty;
+    return result;
+  }
 
-	@override
-	Future<Null> ngOnInit() async {
-		if (null == bookingItem) {
-			print('Failed to initialize popup - no item!');
-			return;
-		}
+  bool get hasProducts => null != booking && booking.products.isNotEmpty;
 
-		try {
-			final client = _clientFactory.getClient();
-			_cabins = await _cruiseRepository.getActiveCruiseCabins(client);
-			_products = await _cruiseRepository.getActiveCruiseProducts(client);
-			booking = await _bookingRepository.getBooking(client, bookingItem.reference);
-		} catch (e) {
-			print('Failed to load booking: ${e.toString()}');
-			// Just ignore this here, we will be stuck in the loading state
-		}
-	}
+  @override
+  Future<void> ngOnInit() async {
+    if (null == bookingItem) {
+      print('Failed to initialize popup - no item!');
+      return;
+    }
+
+    try {
+      final client = _clientFactory.getClient();
+      _cabins = await _cruiseRepository.getActiveCruiseCabins(client);
+      _products = await _cruiseRepository.getActiveCruiseProducts(client);
+      booking = await _bookingRepository.getBooking(client, bookingItem.reference);
+    } catch (e) {
+      print('Failed to load booking: ${e.toString()}');
+      // Just ignore this here, we will be stuck in the loading state
+    }
+  }
 }
 
 @Component(
-	selector: 'booking-preview-component',
-	templateUrl: 'booking_preview_component.html',
-	styleUrls: ['../content/content_styles.css', 'booking_preview_component.css'],
-	directives: <dynamic>[BookingPreviewComponentPopup, sjoslagetMaterialDirectives, ModalComponent]
-)
+    selector: 'booking-preview-component',
+    templateUrl: 'booking_preview_component.html',
+    styleUrls: ['../content/content_styles.css', 'booking_preview_component.css'],
+    directives: <dynamic>[BookingPreviewComponentPopup, sjoslagetMaterialDirectives, ModalComponent])
 class BookingPreviewComponent {
-	BookingOverviewItem booking;
-	bool isOpen;
+  BookingOverviewItem booking;
+  bool isOpen;
 
-	void close() {
-		isOpen = false;
-	}
+  void close() {
+    isOpen = false;
+  }
 
-	void open(BookingOverviewItem booking) {
-		this.booking = booking;
-		isOpen = true;
-	}
+  void open(BookingOverviewItem booking) {
+    this.booking = booking;
+    isOpen = true;
+  }
 }
