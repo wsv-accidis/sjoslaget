2219a77354ffb87c444394f2f0d5eeba99ae74d7
Wilhelm Svenselius <wilhelm.svenselius@gmail.com>
2021-07-10 22:41:42 +0200

    Fix subcruises not reflected correctly in stats. Temporarily move products to second column in stats.

--- SjoslagetFrontend/lib/admin/stats_component.dart
+++ SjoslagetFrontend/lib/admin/stats_component.dart
@@ -13,235 +13,183 @@ import '../model/cruise_product.dart';
 import '../model/report_data.dart';
 import '../model/report_summary.dart';
 import '../model/string_pair.dart';
+import '../model/sub_cruise.dart';
 
 @Component(
-	selector: 'sjoslaget-stats-app',
-	templateUrl: 'stats_component.html',
-	styleUrls: ['stats_component.css'],
-	providers: <dynamic>[
-		ClientFactory,
-		CruiseRepository,
-		ReportRepository
-	],
-	directives: <dynamic>[coreDirectives]
-)
+    selector: 'sjoslaget-stats-app',
+    templateUrl: 'stats_component.html',
+    styleUrls: ['../content/content_styles.css', 'admin_styles.css', 'stats_component.css'],
+    providers: <dynamic>[ClientFactory, CruiseRepository, ReportRepository],
+    directives: <dynamic>[coreDirectives])
 class StatsComponent implements OnInit {
-	static const int NOT_LIMITED = -1;
-	static final NumberFormat _fpNumFormat = NumberFormat('0.00');
+  static const int NOT_LIMITED = -1;
+  static final NumberFormat _fpNumFormat = NumberFormat('0.00');
 
-	final ClientFactory _clientFactory;
-	final CruiseRepository _cruiseRepository;
-	final ReportRepository _reportRepository;
+  final ClientFactory _clientFactory;
+  final CruiseRepository _cruiseRepository;
+  final ReportRepository _reportRepository;
 
-	Map<String, int> cabinsAvailability;
-	List<CruiseCabin> cabins;
-	ReportData current;
-	Map<String, int> productsQuantity;
-	List<CruiseProduct> products;
-	ReportSummary summary;
+  Map<String, int> cabinsAvailability;
+  List<CruiseCabin> cabins;
+  ReportData current;
+  Map<String, int> productsQuantity;
+  List<CruiseProduct> products;
+  List<SubCruise> subCruises;
+  ReportSummary summary;
 
-	StatsComponent(this._clientFactory, this._cruiseRepository, this._reportRepository);
+  StatsComponent(this._clientFactory, this._cruiseRepository, this._reportRepository);
 
-	bool get hasCabins => null != cabins && null != cabinsAvailability;
+  bool get hasCabins => null != cabins && null != cabinsAvailability;
 
-	bool get hasProducts => null != products && null != productsQuantity;
+  bool get hasProducts => null != products && null != productsQuantity;
 
-	bool get hasTopContacts => null != current && current.topGroups.isNotEmpty;
+  bool get hasTopContacts => null != current && current.topGroups.isNotEmpty;
 
-	bool get hasTopGroups => null != current && current.topContacts.isNotEmpty;
+  bool get hasTopGroups => null != current && current.topContacts.isNotEmpty;
 
-	@override
-	Future<Null> ngOnInit() async {
-		try {
-			final client = _clientFactory.getClient();
-			cabinsAvailability = await _cruiseRepository.getCabinsAvailability(client);
-			cabins = await _cruiseRepository.getActiveCruiseCabins(client);
+  @override
+  Future<void> ngOnInit() async {
+    try {
+      final client = _clientFactory.getClient();
+      cabinsAvailability = await _cruiseRepository.getCabinsAvailability(client);
+      cabins = await _cruiseRepository.getActiveCruiseCabins(client);
+      subCruises = await _cruiseRepository.getActiveSubCruises(client);
 
-			products = await _cruiseRepository.getActiveCruiseProducts(client);
-			productsQuantity = await _cruiseRepository.getProductsQuantity(client);
+      products = await _cruiseRepository.getActiveCruiseProducts(client);
+      productsQuantity = await _cruiseRepository.getProductsQuantity(client);
 
-			current = await _reportRepository.getCurrent(client);
-			summary = await _reportRepository.getSummary(client);
-		} catch (e) {
-			print('Failed to load stats: ${e.toString()}');
-			rethrow;
-		}
+      current = await _reportRepository.getCurrent(client);
+      summary = await _reportRepository.getSummary(client);
+    } catch (e) {
+      print('Failed to load stats: ${e.toString()}');
+      rethrow;
+    }
 
-		_createCharts();
-	}
+    _createCharts();
+  }
 
-	int getBookedCount(CruiseCabin cabin) {
-		if (null == cabinsAvailability || !cabinsAvailability.containsKey(cabin.id))
-			return cabin.count;
-		return cabin.count - cabinsAvailability[cabin.id];
-	}
+  int getBookedCount(CruiseCabin cabin) {
+    if (null == cabinsAvailability || !cabinsAvailability.containsKey(cabin.id)) return cabin.count;
+    return cabin.count - cabinsAvailability[cabin.id];
+  }
 
-	int getBookedPercent(CruiseCabin cabin) => 100 * getBookedCount(cabin) ~/ cabin.count;
+  int getBookedPercent(CruiseCabin cabin) => 100 * getBookedCount(cabin) ~/ cabin.count;
 
-	int getProductBookedCount(CruiseProduct prod) {
-		if (null == productsQuantity || !productsQuantity.containsKey(prod.id))
-			return 0;
-		return productsQuantity[prod.id];
-	}
+  List<CruiseCabin> getCabins(String subCruise) => cabins.where((c) => c.subCruise == subCruise).toList(growable: false);
 
-	String getProductCount(CruiseProduct prod) {
-		if (NOT_LIMITED == prod.count)
-			return 'Obegr.';
-		return prod.count.toString();
-	}
+  int getProductBookedCount(CruiseProduct prod) {
+    if (null == productsQuantity || !productsQuantity.containsKey(prod.id)) return 0;
+    return productsQuantity[prod.id];
+  }
 
-	String getProductRatio(CruiseProduct prod) {
-		if (null == summary || summary.paxTotal.isEmpty) {
-			return '-';
-		}
+  String getProductCount(CruiseProduct prod) {
+    if (NOT_LIMITED == prod.count) return 'Obegr.';
+    return prod.count.toString();
+  }
 
-		final int currentPaxTotal = summary.paxTotal.last;
-		return _fpNumFormat.format(getProductBookedCount(prod) / currentPaxTotal);
-	}
+  String getProductRatio(CruiseProduct prod) {
+    if (null == summary || summary.paxTotal.isEmpty) {
+      return '-';
+    }
 
-	void _createCharts() {
-		if (null != summary && summary.labels.isNotEmpty) {
-			_createSimpleLineChart(summary.bookingsCreated, 'Nya bokningar', 'rgba(2,97,163,0.6)', '#bookings-created-chart');
-			_createSimpleLineChart(summary.bookingsTotal, 'Totalt antal bokningar', 'rgba(21,33,112,0.6)', '#bookings-total-chart');
-			_createSimpleLineChart(summary.cabinsTotal, 'Totalt antal hytter', 'rgba(201,40,9,0.6)', '#cabins-total-chart');
-			_createPaxAndCapacityChart('#pax-total-chart');
-		}
+    final int currentPaxTotal = summary.paxTotal.last;
+    return _fpNumFormat.format(getProductBookedCount(prod) / currentPaxTotal);
+  }
 
-		if (null != current) {
-			_createGendersChart('#genders-chart');
-			_createAgeChart('#ages-chart');
-			_createPaymentsChart('#payment-chart');
-		}
-	}
+  void _createCharts() {
+    if (null != summary && summary.labels.isNotEmpty) {
+      _createSimpleLineChart(summary.bookingsCreated, 'Nya bokningar', 'rgba(2,97,163,0.6)', '#bookings-created-chart');
+      _createSimpleLineChart(summary.bookingsTotal, 'Totalt antal bokningar', 'rgba(21,33,112,0.6)', '#bookings-total-chart');
+      _createSimpleLineChart(summary.cabinsTotal, 'Totalt antal hytter', 'rgba(201,40,9,0.6)', '#cabins-total-chart');
+      _createPaxAndCapacityChart('#pax-total-chart');
+    }
 
-	void _createAgeChart(String selector) {
-		if (current.ageDistribution.isEmpty)
-			return;
+    if (null != current) {
+      _createGendersChart('#genders-chart');
+      _createAgeChart('#ages-chart');
+      _createPaymentsChart('#payment-chart');
+    }
+  }
 
-		final data = LinearChartData(
-			labels: current.ageDistribution.map((pair) => pair.key).toList(growable: false),
-			datasets: <ChartDataSets>[
-				ChartDataSets(
-					backgroundColor: 'rgba(21,33,112,0.8)',
-					data: current.ageDistribution.map((pair) => pair.value).toList(growable: false)
-				)
-			]
-		);
+  void _createAgeChart(String selector) {
+    if (current.ageDistribution.isEmpty) return;
 
-		final config = ChartConfiguration(
-			data: data,
-			type: 'bar',
-			options: ChartOptions(
-				legend: ChartLegendOptions(display: false),
-				responsive: true,
-				title: _createTitle('Födelseår')
-			)
-		);
+    final data = LinearChartData(labels: current.ageDistribution.map((pair) => pair.key).toList(growable: false), datasets: <ChartDataSets>[
+      ChartDataSets(backgroundColor: 'rgba(21,33,112,0.8)', data: current.ageDistribution.map((pair) => pair.value).toList(growable: false))
+    ]);
 
-		Chart(querySelector(selector), config);
-	}
+    final config = ChartConfiguration(
+        data: data, type: 'bar', options: ChartOptions(legend: ChartLegendOptions(display: false), responsive: true, title: _createTitle('Födelseår')));
 
-	void _createDoughnutChart(LinearChartData data, String label, String selector) {
-		final config = ChartConfiguration(
-			data: data,
-			type: 'doughnut',
-			options: ChartOptions(
-				legend: ChartLegendOptions(position: 'bottom'),
-				responsive: true,
-				title: _createTitle(label)
-			)
-		);
+    Chart(querySelector(selector), config);
+  }
 
-		Chart(querySelector(selector), config);
-	}
+  void _createDoughnutChart(LinearChartData data, String label, String selector) {
+    final config = ChartConfiguration(
+        data: data, type: 'doughnut', options: ChartOptions(legend: ChartLegendOptions(position: 'bottom'), responsive: true, title: _createTitle(label)));
 
-	void _createGendersChart(String selector) {
-		if (current.genders.isEmpty)
-			return;
+    Chart(querySelector(selector), config);
+  }
 
-		final data = LinearChartData(
-			labels: <String>['Män', 'Övriga', 'Kvinnor'],
-			datasets: <ChartDataSets>[
-				ChartDataSets(
-					backgroundColor: <String>['rgb(119,158,203)', 'rgb(203,119,200)', 'rgb(203,122,119)'],
-					data: <int>[_getValue(current.genders, 'm'), _getValue(current.genders, 'x'), _getValue(current.genders, 'f')]
-				)
-			]
-		);
+  void _createGendersChart(String selector) {
+    if (current.genders.isEmpty) return;
 
-		_createDoughnutChart(data, 'Kön', selector);
-	}
+    final data = LinearChartData(labels: <String>[
+      'Män',
+      'Övriga',
+      'Kvinnor'
+    ], datasets: <ChartDataSets>[
+      ChartDataSets(
+          backgroundColor: <String>['rgb(119,158,203)', 'rgb(203,119,200)', 'rgb(203,122,119)'],
+          data: <int>[_getValue(current.genders, 'm'), _getValue(current.genders, 'x'), _getValue(current.genders, 'f')])
+    ]);
 
-	void _createPaymentsChart(String selector) {
-		if (current.bookingsByPayment.isEmpty)
-			return;
+    _createDoughnutChart(data, 'Kön', selector);
+  }
 
-		final data = LinearChartData(
-			labels: <String>['Ej betalade', 'Betalade'],
-			datasets: <ChartDataSets>[
-				ChartDataSets(
-					backgroundColor: <String>['rgb(201,136,98)', 'rgb(117,209,132)'],
-					data: <int>[_getValue(current.bookingsByPayment, 'unpaid'), _getValue(current.bookingsByPayment, 'paid')]
-				)
-			]
-		);
+  void _createPaymentsChart(String selector) {
+    if (current.bookingsByPayment.isEmpty) return;
 
-		_createDoughnutChart(data, 'Betalning', selector);
-	}
+    final data = LinearChartData(labels: <String>[
+      'Ej betalade',
+      'Betalade'
+    ], datasets: <ChartDataSets>[
+      ChartDataSets(
+          backgroundColor: <String>['rgb(201,136,98)', 'rgb(117,209,132)'],
+          data: <int>[_getValue(current.bookingsByPayment, 'unpaid'), _getValue(current.bookingsByPayment, 'paid')])
+    ]);
 
-	void _createLineChart(LinearChartData data, String label, String selector) {
-		final config = ChartConfiguration(
-			data: data,
-			type: 'line',
-			options: ChartOptions(
-				legend: ChartLegendOptions(display: false),
-				responsive: true,
-				title: _createTitle(label)
-			)
-		);
+    _createDoughnutChart(data, 'Betalning', selector);
+  }
 
-		Chart(querySelector(selector), config);
-	}
+  void _createLineChart(LinearChartData data, String label, String selector) {
+    final config = ChartConfiguration(
+        data: data, type: 'line', options: ChartOptions(legend: ChartLegendOptions(display: false), responsive: true, title: _createTitle(label)));
 
-	void _createPaxAndCapacityChart(String selector) {
-		final data = LinearChartData(labels: summary.labels, datasets: <ChartDataSets>[
-			ChartDataSets(
-				label: 'Totalt antal deltagare',
-				backgroundColor: 'rgba(58,199,151,0.6)',
-				data: summary.paxTotal),
-			ChartDataSets(
-				label: 'Total bokad kapacitet',
-				backgroundColor: 'rgba(21,112,98,0.2)',
-				data: summary.capacityTotal
-			)
-		]);
+    Chart(querySelector(selector), config);
+  }
 
-		_createLineChart(data, 'Totalt antal deltagare/kapacitet', selector);
-	}
+  void _createPaxAndCapacityChart(String selector) {
+    final data = LinearChartData(labels: summary.labels, datasets: <ChartDataSets>[
+      ChartDataSets(label: 'Totalt antal deltagare', backgroundColor: 'rgba(58,199,151,0.6)', data: summary.paxTotal),
+      ChartDataSets(label: 'Total bokad kapacitet', backgroundColor: 'rgba(21,112,98,0.2)', data: summary.capacityTotal)
+    ]);
 
-	void _createSimpleLineChart(List<int> values, String label, String backgroundColor, String selector) {
-		final data = LinearChartData(labels: summary.labels, datasets: <ChartDataSets>[
-			ChartDataSets(
-				backgroundColor: backgroundColor,
-				data: values,
-				label: label
-			)
-		]);
+    _createLineChart(data, 'Totalt antal deltagare/kapacitet', selector);
+  }
 
-		_createLineChart(data, label, selector);
-	}
+  void _createSimpleLineChart(List<int> values, String label, String backgroundColor, String selector) {
+    final data =
+        LinearChartData(labels: summary.labels, datasets: <ChartDataSets>[ChartDataSets(backgroundColor: backgroundColor, data: values, label: label)]);
 
-	ChartTitleOptions _createTitle(String label) =>
-		ChartTitleOptions(display: true,
-			fontColor: 'black',
-			fontFamily: 'Raleway',
-			fontSize: 20,
-			padding: 15,
-			text: label
-		);
+    _createLineChart(data, label, selector);
+  }
 
-	int _getValue(List<KeyValuePair> source, String key) {
-		final pair = source.firstWhere((KeyValuePair pair) => pair.key == key, orElse: () => null);
-		return null != pair ? pair.value : 0;
-	}
+  ChartTitleOptions _createTitle(String label) => ChartTitleOptions(
+      display: true, fontColor: 'black', fontFamily: 'Raleway, Helvetica, Arial, sans-serif', fontSize: 20, padding: 15, fontStyle: 'normal', text: label);
+
+  int _getValue(List<KeyValuePair> source, String key) {
+    final pair = source.firstWhere((KeyValuePair pair) => pair.key == key, orElse: () => null);
+    return null != pair ? pair.value : 0;
+  }
 }
