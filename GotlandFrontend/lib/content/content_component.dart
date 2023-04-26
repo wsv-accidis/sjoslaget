28eab5fa84cf6771d0e1226586c44a06c1d1be32
Wilhelm Svenselius <wilhelm.svenselius@gmail.com>
2023-04-26 22:29:55 +0200

    Healthcare page. Content updates before go time.

--- GotlandFrontend/lib/content/content_component.dart
+++ GotlandFrontend/lib/content/content_component.dart
@@ -2,16 +2,23 @@ import 'package:angular/angular.dart';
 import 'package:angular_router/angular_router.dart';
 
 import 'about_component.template.dart';
+import 'about_routes.dart';
 import 'booking_page.template.dart';
 import 'contact_page.template.dart';
 import 'content_routes.dart';
 import 'day_booking_page.template.dart';
+import 'healthcare_page.template.dart';
 import 'not_found_page.template.dart';
 import 'pricing_page.template.dart';
 import 'solo_booking_page.template.dart';
 import 'start_page.template.dart';
 
-@Component(selector: 'content-component', styleUrls: ['content_component.css'], templateUrl: 'content_component.html', directives: <dynamic>[routerDirectives], exports: [ContentRoutes])
+@Component(
+    selector: 'content-component',
+    styleUrls: ['content_component.css'],
+    templateUrl: 'content_component.html',
+    directives: <dynamic>[routerDirectives],
+    exports: [AboutRoutes, ContentRoutes])
 class ContentComponent {
   final List<RouteDefinition> routes = [
     RouteDefinition(routePath: ContentRoutes.start, component: StartPageNgFactory),
@@ -19,6 +26,7 @@ class ContentComponent {
     RouteDefinition(routePath: ContentRoutes.booking, component: BookingPageNgFactory),
     RouteDefinition(routePath: ContentRoutes.contact, component: ContactPageNgFactory),
     RouteDefinition(routePath: ContentRoutes.day, component: DayBookingPageNgFactory),
+    RouteDefinition(routePath: ContentRoutes.healthcare, component: HealthcarePageNgFactory),
     RouteDefinition(routePath: ContentRoutes.pricing, component: PricingPageNgFactory),
     RouteDefinition(routePath: ContentRoutes.solo, component: SoloBookingPageNgFactory),
     RouteDefinition(path: '.+', component: NotFoundPageNgFactory),
---
+++ GotlandFrontend/lib/content/healthcare_page.dart
@@ -0,0 +1,7 @@
+import 'package:angular/angular.dart';
+
+@Component(
+    selector: 'healthcare-page',
+    styleUrls: ['content_styles.css', 'healthcare_page.css'],
+    templateUrl: 'healthcare_page.html')
+class HealthcarePage {}
