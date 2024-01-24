296171481514e110d2423388a6395129ea86203d
Wilhelm Svenselius <wilhelm.svenselius@gmail.com>
2024-01-24 21:10:20 +0100

    Content updates.

--- GotlandFrontend/lib/content/start_page.dart
+++ GotlandFrontend/lib/content/start_page.dart
@@ -1,10 +1,14 @@
 import 'package:angular/angular.dart';
+import 'package:angular_router/angular_router.dart';
 
 import '../widgets/post_feed.dart';
+import 'about_routes.dart';
+import 'content_routes.dart';
 
 @Component(
     selector: 'start-page',
     styleUrls: ['content_styles.css', 'start_page.css'],
     templateUrl: 'start_page.html',
-    directives: <dynamic>[coreDirectives, PostFeed])
+    directives: <dynamic>[coreDirectives, routerDirectives, PostFeed],
+    exports: [AboutRoutes, ContentRoutes])
 class StartPage {}
