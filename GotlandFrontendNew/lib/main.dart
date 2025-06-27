83370abc9c40861398763be6efed9050ad4a164c
Wilhelm Svenselius <wilhelm.svenselius@gmail.com>
2025-06-27 21:16:17 +0200

    Routing example with go_router.

--- GotlandFrontendNew/lib/main.dart
+++ GotlandFrontendNew/lib/main.dart
@@ -1,20 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+import 'package:go_router/go_router.dart';
+import 'package:gotland_frontend/one_screen.dart';
+import 'package:gotland_frontend/two_screen.dart';
 
 void main() {
+  // TODO: Verify that this works on IIS with existing/updated Web.Config
+  usePathUrlStrategy();
   runApp(const MainApp());
 }
+
+// For docs, see https://pub.dev/documentation/go_router/latest/topics/Configuration-topic.html
+final _mainRouter = GoRouter(
+  routes: [
+    GoRoute(path: '/', builder: (context, state) => OneScreen()),
+    GoRoute(path: '/two', builder: (context, state) => TwoScreen()),
+  ],
+);
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
+    return MaterialApp.router(routerConfig: _mainRouter);
   }
 }
---
+++ GotlandFrontendNew/lib/two_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class TwoScreen extends StatelessWidget {
+  const TwoScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(body: Center(child: Text('Two Screens!'))),
+    );
+  }
+}
