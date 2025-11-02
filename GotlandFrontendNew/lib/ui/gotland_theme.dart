3af78d87f2c66f08e63f2905425c4c0c4ed51a53
Wilhelm Svenselius <wilhelm.svenselius@gmail.com>
2025-11-03 00:35:35 +0100

    Improve text style.

--- GotlandFrontendNew/lib/ui/gotland_theme.dart
+++ GotlandFrontendNew/lib/ui/gotland_theme.dart
@@ -3,12 +3,14 @@ import 'package:flutter/material.dart';
 class GotlandTheme {
   static ThemeData lightThemeData(BuildContext context) {
     return ThemeData(
-      useMaterial3: true,
+      fontFamily: 'Roboto',
+      textTheme: TextTheme.of(context).apply(fontSizeFactor: 1.2, letterSpacingDelta: -0.5, heightFactor: 0.9),
       colorScheme: ColorScheme.fromSeed(
         surface: Colors.white,
         seedColor: Color.fromARGB(255, 0, 153, 0),
         dynamicSchemeVariant: DynamicSchemeVariant.content,
       ),
+      useMaterial3: true,
     );
   }
 }
